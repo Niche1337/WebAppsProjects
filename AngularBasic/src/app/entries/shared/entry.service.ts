5fb9a12b43619026dafb029bf516285296ad0e1a
Nitesh Parmar <niteshp2007@gmail.com>
2020-01-30 22:50:29 +0000

    Photo Blog
    A basic app using angular

---
+++ AngularBasic/src/app/entries/shared/entry.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Entry } from './entry.model';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class EntryService {
+
+    constructor(private http: Http){
+        
+    }
+
+    addComment(entryId: number, comment: { name: string; comment:string;}) {
+        return this.http.post(`/app/entries/${entryId}/comments`, comment)
+            .toPromise();
+    }
+
+    getEntries(): Promise<Entry[]> { 
+        return this.http.get('/app/entries')
+                        .toPromise()
+                        .then(response => response.json().data as Entry[]);
+    }
+}
---
+++ AngularBasic/wwwroot/app.90c2985c89049f31e75b.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],[function(t,e,c){"use strict";c(1)},function(t,e){t.exports="// removed by extract-text-webpack-plugin"}]);
+//# sourceMappingURL=app.90c2985c89049f31e75b.js.map
